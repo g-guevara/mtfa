ed3c0a733ce2b70791173a83d08f126bf05b74f2
gguevara112 <guguevara@alumnos.uai.cl>
2025-03-07 21:05:41 -0300

    croped image

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       <div className="image-wrapper">
         {/* Imagen de fondo */}
         <Image
-          src="/intro4.png"
+          src="/intro42.png"
           alt="Imagen con cuadrícula"
           className="image"
           width={3280}
