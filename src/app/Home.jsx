e58aa106a74075d2db1a395e2c1398af98266e5a
gguevara112 <guguevara@alumnos.uai.cl>
2025-03-09 15:19:38 -0300

    edu section

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -9,7 +9,7 @@ export default function HomeContent() {
   // Estado con los textos y sus posiciones, ahora con rutas internas
   const [texts, setTexts] = useState([
     { id: 1, x: 10, y: 52, text: "Education", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/education" },
-    { id: 2, x: 20, y: 69, text: "Interests", rotation: 0, scaleRotation: 0, rotationDirection: 1, url: "/pagina2" },
+    { id: 2, x: 19, y: 69, text: "Interests", rotation: 0, scaleRotation: 0, rotationDirection: 1, url: "/pagina2" },
     { id: 3, x: 65, y: 80, text: "Skills", rotation: 90, scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
     { id: 4, x: 25, y: 10, text: "Contact", rotation: 66, scaleRotation: 0.06, rotationDirection: 1, url: "/pagina2" },
     { id: 5, x: 65, y: 9, text: "Experience", rotation: -55, scaleRotation: 0.06, rotationDirection: -1, url: "/pagina2" },
--- src/app/education/Edu.jsx
+++ src/app/education/Edu.jsx
@@ -9,12 +9,9 @@ export default function HomeContent() {
   // Estado con los textos y sus posiciones, ahora con rutas internas
   const [texts, setTexts] = useState([
     { id: 1, x: 10, y: 52, text: "Education", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/education" },
-    { id: 2, x: 20, y: 69, text: "Interests", rotation: 0, scaleRotation: 0, rotationDirection: 1, url: "/pagina2" },
-    { id: 3, x: 65, y: 80, text: "Skills", rotation: 90, scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
-    { id: 4, x: 25, y: 10, text: "Contact", rotation: 66, scaleRotation: 0.06, rotationDirection: 1, url: "/pagina2" },
-    { id: 5, x: 65, y: 9, text: "Experience", rotation: -55, scaleRotation: 0.06, rotationDirection: -1, url: "/pagina2" },
-    { id: 6, x: 84, y: 10, text: "Languages", rotation: 90, scaleRotation: 0, rotationDirection: 1, url: "/pagina2" },
-    { id: 7, x: 87.7, y: 50, text: "Projects", rotation: 55, scaleRotation: 0.06, rotationDirection: 1, url: "/pagina2" },
+    { id: 2, x: 22, y: 97, text: "❬ Back", rotation: 0, scaleRotation: 0, rotationDirection: 1, url: "/" },
+    { id: 3, x: 68, y:24, text: "Universidad Adolfo Ibañez", rotation: -34.5, scaleRotation: 0.06, rotationDirection: -1, url: "/pagina2" },
+    { id: 4, x: 98.5, y: 49, text: "2021 - present", rotation: 90, scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
   ]);
   
   
@@ -25,7 +22,7 @@ export default function HomeContent() {
       <div className="image-wrapper">
         {/* Imagen de fondo */}
         <Image
-          src="/imgEdu.png"
+          src="/imgEdu2.png"
           alt="Imagen con cuadrícula"
           className="image"
           width={3280}
