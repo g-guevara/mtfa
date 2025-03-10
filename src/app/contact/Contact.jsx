5b349084439c9eda54a5992361e6e8b6f1753df6
gguevara112 <guguevara@alumnos.uai.cl>
2025-03-09 22:04:52 -0300

    good

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -6,25 +6,19 @@ import GridOverlay from "./GridOverlay";
 import "./Home.css";
 
 export default function HomeContent() {
-  // Estado con los textos y sus posiciones, ahora con rutas internas
   const [texts, setTexts] = useState([
     { id: 1, x: 85, y: 97, text: "Spanish", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/education" },
     { id: 2, x: 4, y: 97, text: "❬ Back", rotation: 0, scaleRotation: 0, rotationDirection: 1, url: "/" },
-  //  { id: 3, x: 22, y:4, text: "Enlgish", rotation: 0., scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
-    { id: 4, x: 98.5, y: 42, text: "Italian", rotation: 90, scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
+    { id: 4, x: 84, y: 4, text: "guevara.guillermo.g@gmail.com", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/pagina2" },
     { id: 5, x: 25, y: 10, text: "Contact", rotation: 66, scaleRotation: 0.06, rotationDirection: 1, url: "/contact" },
-
   ]);
-  
-  
-  
 
   return (
     <div className="full-screen-container">
       <div className="image-wrapper">
-        {/* Imagen de fondo */}
+        {/* Imagen de fondo - NO SE TOCA */}
         <Image
-          src="/contact3.png"
+          src="/contact4.png"
           alt="Imagen con cuadrícula"
           className="image"
           width={3280}
@@ -33,7 +27,19 @@ export default function HomeContent() {
           unoptimized
         />
 
-        {/* Cuadrícula interactiva */}
+        {/* Imagen sobrepuesta - SE AGREGA SIN AFECTAR LO DEMÁS */}
+        <Image
+          src="/portait.jpg"
+          alt="Imagen sobrepuesta"
+          className="overlay-image"
+          width={500} /* Ajusta el tamaño si es necesario */
+          height={700}
+          priority
+          unoptimized
+        />
+        
+
+        {/* Cuadrícula interactiva - NO SE TOCA */}
         <GridOverlay texts={texts} />
       </div>
     </div>
