c4358f6ab81984b313bd4e76fb08cdad27901597
gguevara112 <guguevara@alumnos.uai.cl>
2025-03-11 10:27:44 -0300

    ready experience

--- src/app/experience/Experience.jsx
+++ src/app/experience/Experience.jsx
@@ -9,11 +9,10 @@ export default function HomeContent() {
   // Estado con los textos y sus posiciones, ahora con rutas internas
   const [texts, setTexts] = useState([
     { id: 1, x: 25, y: 42, text: "UAI Photovoltaic Laboratory UAI", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/education" },
-    { id: 32, x: 33, y: 59, text: "- Assisted two magistrates in the preparation of their thesis, and optimization and maintenance of the photovoltaic plant, working with suppliers and professors.", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "" },
     { id: 233, x: 26, y: 49, text: ["March 2022 - March 2023, ", <b key="assistant">Assistant</b>], rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/" },
 
     { id: 13, x: 86, y: 20, text: "Gildemeister Chile", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "https://www.gildemeister.cl/" },
-    { id: 332, x: 33, y: 59, text: "- Assisted two magistrates in the preparation of their thesis, and optimization and maintenance of the photovoltaic plant, working with suppliers and professors.", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "" },
+    { id: 332, x: 32.5, y: 59, text: "Assisted two magistrates in the preparation of their thesis, and optimization and maintenance of the photovoltaic plant, working with suppliers and professors.", rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "" },
     { id: 2333, x: 87, y: 27, text: ["February 2025, ", <b key="assistant">Intern</b>], rotation: 0, scaleRotation: 0, rotationDirection: -1, url: "/" },
     
 
@@ -33,7 +32,7 @@ export default function HomeContent() {
       <div className="image-wrapper">
         {/* Imagen de fondo */}
         <Image
-          src="/exp2.png"
+          src="/exp20.png"
           alt="Imagen con cuadrícula"
           className="image"
           width={3280}
